467a6eea989a69fdebba9d00ac88cae717faed01
Kenneth A Esdaile <kanthonye@Kenneths-MBP.fios-router.home>
2019-08-13 20:39:28 -0400

    aug 13

--- Uniform.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Uniform.cpp
-//  dev-kege
-//
-//  Created by Kenneth A Esdaile on 5/5/19.
-//  Copyright © 2019 Kenneth A Esdaile. All rights reserved.
-//
-
-#include "../../graphics/framework/Uniform.hpp"
